5398b64d6b02b67766234d907f7b77115d136d11
MaksimSolo <taezhnyi@gmail.com>
2022-09-03 15:03:56 +0300

    #14.4 - lesson6: LearnJS part 1,2,3,4,6 +tasks: done

--- src/lessons/lesson6/lesson6.js
+++ src/lessons/lesson6/lesson6.js
@@ -365,6 +365,32 @@ console.log('Lesson 6');
 //     Таким образом, по логике instanceof, именно prototype в действительности определяет тип, а не функция-конструктор.
 
 
+// class CoffeeMachine {
+//     _waterAmount = 0;
+//
+//     set waterAmount(value) {
+//         if (value < 0) throw new Error("Отрицательное количество воды");
+//         this._waterAmount = value;
+//     }
+//
+//     get waterAmount() {
+//         return this._waterAmount;
+//     }
+//
+//     constructor(power) {
+//         this._power = power;
+//     }
+//
+// }
+//
+// // создаём новую кофеварку
+// let coffeeMachine = new CoffeeMachine(100);
+//
+// // устанавливаем количество воды
+// // coffeeMachine.waterAmount = -10; // Error: Отрицательное количество воды
+// coffeeMachine.waterAmount = 100
+// console.log(coffeeMachine.waterAmount)
+
 
 
 
